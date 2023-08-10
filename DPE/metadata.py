b3a90407261b2a90e041eff0a67b2b098e24d81c
ibnaleem <ibnaleem@outlook.com>
2023-08-10 11:17:38 +0000

    create random name generator

--- DPE/metadata.py
+++ DPE/metadata.py
@@ -1,5 +1,13 @@
-import subprocess
+import subprocess, requests, string
 from exiftool import ExifTool
+
+def _generate_name() -> str:
+    """ Generates random names for metadata-masking"""
+    r = requests.get('https://api.namefake.com/english-united-states/')
+    name = r.json()["name"]
+
+    return str(name)
+
 
 def read_metadata(path: str) -> list:
     """Returns a list of dictionaries containing all metadata found in file"""
@@ -20,3 +28,4 @@ def erase_metadata(path: str) -> str:
     
     except Exception as e:
         return str(e)
+

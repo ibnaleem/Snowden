2563c400f24c8d85a8f54bc973a90891762e6440
ibnaleem <ibnaleem@outlook.com>
2023-08-10 13:25:08 +0000

    decryption

--- ACS/password.py
+++ ACS/password.py
@@ -12,3 +12,15 @@ def password_protect(file_path, password) -> bool or str:
     
     except Exception as e:
         return f"Error while password protecting {file_path}: {e}"
+
+def decrypt_protected_file(encrypted_file_path, password) -> bool or str:
+    # Define buffer size for decryption
+    buffer_size = 64 * 1024  # 64KB
+
+    try:
+        decrypted_file_path = encrypted_file_path.replace(".aes", "")
+        pyAesCrypt.decryptFile(encrypted_file_path, decrypted_file_path, password, buffer_size)
+        
+        return True
+    except Exception as e:
+        return f"Error while decrypting {encrypted_file_path}: {e}"

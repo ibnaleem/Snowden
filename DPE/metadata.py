2e8cb8b1d5c8ab0ee83cd746e41b91552b6eda83
ibnaleem <ibnaleem@outlook.com>
2023-08-10 10:52:17 +0000

    Create Metadata Removal Method

--- DPE/metadata.py
+++ DPE/metadata.py
@@ -1,7 +1,7 @@
 import subprocess
 from exiftool import ExifTool
 
-def read_metadata(path:str) -> list:
+def read_metadata(path: str) -> list:
     """Returns a list of dictionaries containing all metadata found in file"""
     try:
         with ExifTool() as et:
@@ -11,3 +11,11 @@ def read_metadata(path:str) -> list:
     except Exception as e:
         return e
 
+def remove_metadata(path: str) -> str:
+    try:
+        with ExifTool() as et:
+            et.execute("-all=", path)
+            return "Metadata removed successfully"
+    
+    except Exception as e:
+        return str(e)

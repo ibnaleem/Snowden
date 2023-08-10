a69e5c11d7dad7930db10832b22d06a57d685abc
ibnaleem <ibnaleem@outlook.com>
2023-08-10 10:15:07 +0000

    Read and Return File Metadata Using

--- DPE/metadata.py
+++ DPE/metadata.py
@@ -1,1 +1,18 @@
+import subprocess
+from exiftool import ExifTool
 
+def read_metadata(path:str) -> list:
+    """Returns a list of dictionaries containing all metadata found in file"""
+    try:
+        with ExifTool() as et:
+            metadata = et.get_metadata(path)
+            return metadata
+
+    except TypeError:
+        return "Provide file path as string"
+
+    except FileNotFoundError:
+        return "File does not exist"
+
+    except subprocess.CalledProcessError:
+        return "Check file permissions or file existence, or run with admin perms"

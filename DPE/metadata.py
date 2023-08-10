88cc8bcdb32cda581027600301ba1975dc157f5b
ibnaleem <ibnaleem@outlook.com>
2023-08-10 11:40:01 +0000

    create metadata masker method

--- DPE/metadata.py
+++ DPE/metadata.py
@@ -7,7 +7,8 @@ def read_metadata(path: str) -> list:
     try:
         with ExifTool() as et:
             metadata = et.get_metadata(path)
-            return metadata
+        
+        return metadata
 
     except Exception as e:
         return e
@@ -17,7 +18,31 @@ def erase_metadata(path: str) -> bool:
     try:
         with ExifTool() as et:
             et.execute("-all=", path)
-            return True
+        
+        return True
     
     except Exception:
         return False
+
+def mask_metadata(path: str) -> bool:
+    """ Overwrites file with fake metadata """
+    try:
+        with ExifTool as et:
+            et.execute("-all=", path) # erase previous metadata
+
+            metadata = {
+                'EXIF:Make': string.ascii_letters + string.digits,
+                'EXIF:Model': string.ascii_letters + string.digits,
+                'XMP:Creator': string.ascii_letters,
+                'XMP:Title': string.ascii_letters + string.digits,
+                # Add more metadata fields as needed
+            }
+
+            for tag, value in metadata.items():
+                et.execute(f"-{tag}={value}", path)
+
+        return True
+
+    except Exception:
+        return False
+            

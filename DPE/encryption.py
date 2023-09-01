7814d7f2215231c37b8d06cf1ca69789f615b68a
ibnaleem <134088573+ibnaleem@users.noreply.github.com>
2023-09-01 10:02:38 +0100

    remove and modify imports

--- DPE/encryption.py
+++ DPE/encryption.py
@@ -1,10 +1,4 @@
-import hashlib
-from base64 import b64encode, b64decode
-from Cryptodome.Cipher import AES
-from Cryptodome.Random import get_random_bytes
-from rich.console import Console
-from rich.table import Table
-from rich.style import Style
+import os, sys, hashlib
 
 encrypted_list = []
 
